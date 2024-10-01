31ae48ea4822a023f0027d6165f5807dc422f146
Alexander Kolokolov <mariokurwaq337@gmail.com>
2024-10-01 22:39:41 +0400

    Добавил ChebyshevPolynomialInterpolation и тесты к ней

---
+++ labsjava/src/main/java/functions/ChebyshevPolynomialInterpolation.java
@@ -0,0 +1,35 @@
+package functions;
+
+public class ChebyshevPolynomialInterpolation implements MathFunction {
+    private MathFunction function;
+    private double[] nodes;
+    private int n;
+    private double a;
+    private double b;
+
+    public ChebyshevPolynomialInterpolation(MathFunction function, double a, double b, int n) {
+        this.function = function;
+        this.n = n;
+        this.a = a;
+        this.b = b;
+        nodes = new double[n + 1];
+        for (int i = 0; i <= n; i++) {
+            nodes[i] =  (b - a) / 2 * Math.cos(Math.PI * (2 * i + 1) / (2 * n + 2)) + (a + b) / 2;
+        }
+    }
+
+    @Override
+    public double apply(double x) {
+        double result = 0;
+        for (int j = 0; j < nodes.length; j++) {
+            double lagrangeBasisPolynomial = function.apply(nodes[j]);
+            for (int k = 0; k < nodes.length; k++) {
+                if (k != j) {
+                    lagrangeBasisPolynomial *= (x - nodes[k]) / (nodes[j] - nodes[k]);
+                }
+            }
+            result += lagrangeBasisPolynomial;
+        }
+        return result;
+    }
+}
