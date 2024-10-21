983898627c97a696583d8d294bb66debe113ecb4
KoTiKo43 <akimnik2005@mail.ru>
2024-10-21 15:30:52 +0400

    add: Добавил бинарные операции сложения и вычитания для TabulatedFunctionOperationService и исключение InconsistentFunctionsException

--- labsjava/src/main/java/operations/TabulatedFunctionOperationService.java
+++ labsjava/src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,30 @@
 package operations;
 
+import exceptions.InconsistentFunctionsException;
 import functions.TabulatedFunction;
 import functions.Point;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] points = new Point[tabulatedFunction.getCount()];
         int i = 0;
@@ -13,5 +34,40 @@ public class TabulatedFunctionOperationService {
             i++;
         }
         return points;
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Functions have different number of points");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[a.getCount()];
+
+        for (int i = 0; i < a.getCount(); i++) {
+            if (pointsA[i].x != pointsB[i].x) {
+                throw new InconsistentFunctionsException("X values of functions do not match");
+            }
+
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u,v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u,v) -> u - v);
     }
 }
--- labsjava/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ labsjava/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,6 +1,10 @@
 package operations;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.LinkedListTabulatedFunctionFactory;
 import org.junit.jupiter.api.Test;
 import functions.ArrayTabulatedFunction;
 import functions.LinkedListTabulatedFunction;
@@ -41,5 +45,54 @@ class TabulatedFunctionOperationServiceTest {
         assertEquals(function.getY(0), points[0].y);
         assertEquals(function.getY(1), points[1].y);
         assertEquals(function.getY(2), points[2].y);
+    }
+
+    @Test
+    public void testAddArrayFunctions() {
+        ArrayTabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(factory);
+
+        ArrayTabulatedFunction function1 = new ArrayTabulatedFunction(new double[]{1, 2, 3}, new double[]{10, 20, 30});
+        ArrayTabulatedFunction function2 = new ArrayTabulatedFunction(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+
+        TabulatedFunction result = service.add(function1, function2);
+
+        assertEquals(function1.getY(0) + function2.getY(0), result.getY(0), 1e-5);
+        assertEquals(function1.getY(1) + function2.getY(1), result.getY(1), 1e-5);
+        assertEquals(function1.getY(2) + function2.getY(2), result.getY(2), 1e-5);
+    }
+
+    @Test
+    public void testSubtractLinkedListFunctions() {
+        LinkedListTabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(factory);
+
+        LinkedListTabulatedFunction function1 = new LinkedListTabulatedFunction(new double[]{1, 2, 3}, new double[]{10, 20, 30});
+        LinkedListTabulatedFunction function2 = new LinkedListTabulatedFunction(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+
+        TabulatedFunction result = service.subtract(function1, function2);
+
+        assertEquals(function1.getY(0) - function2.getY(0), result.getY(0), 1e-5);
+        assertEquals(function1.getY(1) - function2.getY(1), result.getY(1), 1e-5);
+        assertEquals(function1.getY(2) - function2.getY(2), result.getY(2), 1e-5);
+    }
+
+    @Test
+    public void testAddSubtractDifferentFunctions() {
+        LinkedListTabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(factory);
+
+        LinkedListTabulatedFunction function1 = new LinkedListTabulatedFunction(new double[]{1, 2, 3}, new double[]{10, 20, 30});
+        ArrayTabulatedFunction function2 = new ArrayTabulatedFunction(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+
+        TabulatedFunction result1 = service.add(function1, function2);
+        TabulatedFunction result2 = service.subtract(function1, function2);
+
+        assertEquals(function1.getY(0) + function2.getY(0), result1.getY(0), 1e-5);
+        assertEquals(function1.getY(1) + function2.getY(1), result1.getY(1), 1e-5);
+        assertEquals(function1.getY(2) + function2.getY(2), result1.getY(2), 1e-5);
+        assertEquals(function1.getY(0) - function2.getY(0), result2.getY(0), 1e-5);
+        assertEquals(function1.getY(1) - function2.getY(1), result2.getY(1), 1e-5);
+        assertEquals(function1.getY(2) - function2.getY(2), result2.getY(2), 1e-5);
     }
 }
