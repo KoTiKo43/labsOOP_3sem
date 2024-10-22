06ad211253382fddf9e3bbee131eab47f1d770d2
Alexander Kolokolov <mariokurwaq337@gmail.com>
2024-10-22 23:33:22 +0400

    Add: Добавлены в класс FunctionsIO метод readTabulatedFunction и класс TabulatedFunctionFileInputStream.

--- labsjava/src/main/java/io/FunctionsIO.java
+++ labsjava/src/main/java/io/FunctionsIO.java
@@ -2,25 +2,13 @@ package io;
 
 import functions.Point;
 import functions.TabulatedFunction;
-
+import functions.factory.TabulatedFunctionFactory;
 
 import java.io.*;
 
 final public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Class is final");
-    }
-
-    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
-        PrintWriter printWriter = new PrintWriter(writer);
-
-        printWriter.println(function.getCount());
-
-        for (Point point : function) {
-            printWriter.printf("%f %f\n", point.x, point.y);
-        }
-
-        printWriter.flush();
     }
 
     public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
@@ -34,4 +22,17 @@ final public class FunctionsIO {
 
         dataOutputStream.flush();
     }
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException{
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+        int count = dataInputStream.readInt();
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        for (int i = 0; i < count; i++) {
+            xValues[i] = dataInputStream.readDouble();
+            yValues[i] = dataInputStream.readDouble();
+        }
+        return factory.create(xValues, yValues);
+    }
+
 }
