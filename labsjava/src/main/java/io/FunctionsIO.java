ee538dce3fa0bd3be228ce6dddee2f88215d70c2
KoTiKo43 <akimnik2005@mail.ru>
2024-10-22 22:53:37 +0400

    Попытки фиксить

--- labsjava/src/main/java/io/FunctionsIO.java
+++ labsjava/src/main/java/io/FunctionsIO.java
@@ -3,7 +3,9 @@ package io;
 import functions.Point;
 import functions.TabulatedFunction;
 
+
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 final public class FunctionsIO {
@@ -11,12 +13,12 @@ final public class FunctionsIO {
         throw new UnsupportedOperationException("Class is final");
     }
 
-    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
         PrintWriter printWriter = new PrintWriter(writer);
 
         printWriter.println(function.getCount());
 
-        for (Point point: function) {
+        for (Point point : function) {
             printWriter.printf("%f %f\n", point.x, point.y);
         }
 
