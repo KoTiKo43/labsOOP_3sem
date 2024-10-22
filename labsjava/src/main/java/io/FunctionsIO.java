14a1a5b747d06c3b903da4b9d69aff5bd9ff3145
Alexander Kolokolov <mariokurwaq337@gmail.com>
2024-10-22 22:08:45 +0400

    Add: Добавлены в класс FunctionsIO метод writeTabulatedFunction() и класс TabulatedFunctionFileOutputStream.

--- labsjava/src/main/java/io/FunctionsIO.java
+++ labsjava/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,27 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 final public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Class is final");
     }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+
+        dataOutputStream.flush();
+    }
+
 }
