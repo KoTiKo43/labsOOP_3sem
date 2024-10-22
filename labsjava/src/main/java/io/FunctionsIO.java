9e706426377bea88fbf8ae08a26140cba1a99620
KoTiKo43 <akimnik2005@mail.ru>
2024-10-22 22:32:08 +0400

    Обновил .gitignore (не в той папке делал output лол), добавил TabulatedFunctionFileWriter

--- labsjava/src/main/java/io/FunctionsIO.java
+++ labsjava/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,25 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 final public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Class is final");
+    }
+
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        printWriter.println(function.getCount());
+
+        for (Point point: function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+
+        printWriter.flush();
     }
 }
