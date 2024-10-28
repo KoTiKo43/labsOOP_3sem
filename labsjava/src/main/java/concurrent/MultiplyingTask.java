b7c9d74c5703f63c93c17dc232229ef6a3d079f5
KoTiKo43 <akimnik2005@mail.ru>
2024-10-28 22:27:40 +0400

    add: Синхронизировал потоки в MultiplyingTask

--- labsjava/src/main/java/concurrent/MultiplyingTask.java
+++ labsjava/src/main/java/concurrent/MultiplyingTask.java
@@ -12,7 +12,9 @@ public class MultiplyingTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
-            tabulatedFunction.setY(i, tabulatedFunction.getY(i) * 2);
+            synchronized (tabulatedFunction) {
+                tabulatedFunction.setY(i, tabulatedFunction.getY(i) * 2);
+            }
         }
 
         System.out.println("Thread " + Thread.currentThread().getName() + " completed the task.");
