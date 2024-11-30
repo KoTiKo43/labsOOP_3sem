322e88d580ce6a5cc0f67214fb064d52d1de1ec0
Alexander Kolokolov <mariokurwaq337@gmail.com>
2024-11-30 21:36:21 +0400

    Add: Добавил авторизацию

---
+++ labsjava/src/main/java/db/security/user/JwtUtils.java
@@ -0,0 +1,67 @@
+package db.security.user;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secret)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }
+        catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token: {}", e.getMessage());
+        }
+        catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token: {}", e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        }
+        catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        }
+        catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        }
+            return false;
+    }
+}
